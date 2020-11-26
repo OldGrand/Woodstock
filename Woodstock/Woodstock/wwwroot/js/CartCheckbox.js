6d484befcc4c04791154b85c5444002501ede46f
OldGrand <Sergey.Sipov.150699@gmail.com>
2020-11-26 03:52:53 +0300

    commit

---
+++ Woodstock/Woodstock/wwwroot/js/CartCheckbox.js
@@ -0,0 +1,10 @@
+﻿const mainCheck = document.querySelector('#mainCheck');
+
+const childCheckbox = document.querySelectorAll('.cart-checkbox');
+
+mainCheck.addEventListener('change', e => {
+    if (e.target.checked)
+        childCheckbox.forEach(e => e.checked = true);
+    else
+        childCheckbox.forEach(e => e.checked = false);
+});
